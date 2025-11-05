b0d09516adee7733cb48e5c5d8de6e0c90427378
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-05 12:10:10 -0900

    Added functionalit to Editor widget

--- Studio/Editor/src/sunaba/studio/Editor.hx
+++ Studio/Editor/src/sunaba/studio/Editor.hx
@@ -12,12 +12,75 @@ import sunaba.core.Vector2i;
 import sunaba.HorizontalAlignment;
 import sunaba.ui.HSplitContainer;
 import sunaba.ui.TabContainer;
+import sunaba.desktop.Window;
+import sunaba.io.FileSystemIo;
+import sunaba.ui.Control;
+import sunaba.core.native.NativeReference;
+import sunaba.input.InputService;
+import sunaba.core.Callable;
+import sunaba.PlatformService;
+import sunaba.PlatformDeviceType;
+import haxe.Exception;
 
 class Editor extends Widget {
     var sProjPath = "";
 
+    public var haxePath:String = "haxe"; // Default path to Haxe compiler
+
+    public var isGameRunning: Bool = false;
+
+    var leftTabBar: VBoxContainer;
+    var rightTabBar: VBoxContainer;
+
+    var leftTabContainer: TabContainer;
+    var centerTabContainer: TabContainer;
+    var rightTabContainer: TabContainer;
+
+    var leftSidebarChildren: Array<Widget> = [];
+    var rightSidebarChildren: Array<Widget> = [];
+    var workspaceChildern: Array<Widget> = [];
+
+    public var saveFileButton: Button;
+    public var reloadButton: Button;
+    public var buildButton: Button;
+
+    public var playButton:Button;
+    public var pauseButton:Button;
+    public var stopButton:Button;
+
+    public var window:Window;
+    public var windowSize:Vector2i;
+    public var titlebarLmbPressed:Bool = false;
+    public var clickcount = 0;
+    public var timeSinceClick = 0.1;
+    public var windowTitle:Label;
+    public var subtitle:String = "";
+
+    private var projectIo: FileSystemIo;
+
     public override function init() {
         load("studio://Editor.suml");
+
+        leftTabBar = getNodeT(VBoxContainer, "vbox/hbox/leftTabBar");
+        rightTabBar = getNodeT(VBoxContainer, "vbox/hbox/rightTabBar");
+
+        leftTabContainer = getNodeT(TabContainer, "vbox/hbox/hsplit1/leftSidebar");
+        centerTabContainer = getNodeT(TabContainer, "vbox/hbox/hsplit1/hsplit2/workspace");
+        rightTabContainer = getNodeT(TabContainer, "vbox/hbox/hsplit1/hsplit2/rightSidebar");
+
+        saveFileButton = getNodeT(Button, "vbox/toolbar/hbox/leftToolbar/saveFile");
+        reloadButton = getNodeT(Button, "vbox/toolbar/hbox/leftToolbar/reload");
+        buildButton = getNodeT(Button, "vbox/toolbar/hbox/leftToolbar/build");
+
+        playButton = getNodeT(Button, "vbox/toolbar/hbox/rightToolbar/play");
+        pauseButton = getNodeT(Button, "vbox/toolbar/hbox/rightToolbar/pause");
+        stopButton = getNodeT(Button, "vbox/toolbar/hbox/rightToolbar/stop");
+
+        windowTitle = getNodeT(Label, "vbox/menuBarControl/windowTitle");
+        if (OSService.getName() != "macOS") {
+            windowTitle.hide();
+        }
+
     }
 
     public override function onReady() {
@@ -36,6 +99,130 @@ class Editor extends Widget {
         window.moveToCenter();
         //window.extendToTitle = true;
         window.mode = WindowMode.maximized;
+
+        try {
+            trace("hi!");
+            var menuBarControl: Control = getNodeT(Control, "vbox/menuBarControl");
+            trace("");
+            if (OSService.getName() == "macOS") {
+                trace("");
+                var windowSize = null;
+                trace("");
+                var eventFunc = function(eventN: NativeReference) {
+                    if (window == null)
+                        return;
+
+                    if (InputService.isMouseButtonPressed(MouseButton.left) && !titlebarLmbPressed && window.mode == WindowMode.windowed) {
+                        titlebarLmbPressed = true;
+                        window.startDrag();
+                        clickcount++;
+                    }
+                    else if (InputService.isMouseButtonPressed(MouseButton.left) && !titlebarLmbPressed) {
+                        titlebarLmbPressed = true;
+                        clickcount++;
+                    }
+                    else if (!InputService.isMouseButtonPressed(MouseButton.left) && titlebarLmbPressed) {
+                        titlebarLmbPressed = false;
+                    }
+
+                    if (clickcount == 2) {
+                        trace(clickcount);
+                        clickcount = 0;
+                        if (window.mode == WindowMode.maximized) {
+                            window.mode = WindowMode.windowed;
+                        }
+                        else if (window.mode == WindowMode.windowed) {
+                            windowSize = window.size;
+                            window.mode = WindowMode.maximized;
+                        }
+                    }
+                };
+                trace("");
+
+                var menuBar: Control = getNodeT(Control, "vbox/menuBarControl/menuBar");
+                var toolBarSpacer: Control = getNodeT(Control, "vbox/toolbar/hbox/spacer");
+                menuBar.guiInput.connect(eventFunc);
+                menuBarControl.guiInput.connect(eventFunc);
+                toolBarSpacer.guiInput.connect(eventFunc);
+            }
+
+            /*var args = Sys.args();
+            for (arg in args) {
+                if (StringTools.endsWith(arg, ".sproj")) {
+                    sProjPath = arg;
+                    break;
+                }
+            }*/
+
+            var fileMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/File");
+            fileMenu.idPressed.connect(Callable.fromFunction(function(id: Int) {
+                if (id == 0) {
+                    Debug.error("'New File' not implemented");
+                }
+                else if (id == 1) {
+                    Debug.error("'Open File' not implemented");
+                }
+                else if (id == 2) {
+                    Debug.error("'Save File' not implemented");
+                }
+                else if (id == 3) {
+                    Debug.error("'Publish' not implemented");
+                }
+                else if (id == 4) {
+
+                }
+                else if (id == 5) {
+                    Debug.error("'Open Project in Visual Studio Code' not implemented");
+                }
+            }));
+
+            var editMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/Edit");
+            editMenu.idPressed.connect(Callable.fromFunction(function(id: Int) {
+                if (id == 0) {
+                    Debug.error("'Undo' not implemented");
+                }
+                else if (id == 1) {
+                    Debug.error("'Redo' not implemented");
+                }
+                else if (id == 2) {
+                    Debug.error("'Cut' not implemented");
+                }
+                else if (id == 3) {
+                    Debug.error("'Copy' not implemented");
+                }
+                else if (id == 4) {
+                    Debug.error("'Paste' not implemented");
+                }
+            }));
+            var viewMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/View");
+            viewMenu.idPressed.connect(Callable.fromFunction(function(id: Int) {
+
+            }));
+            var toolsMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/Tools");
+            toolsMenu.idPressed.connect(Callable.fromFunction(function(id: Int) {
+
+            }));
+            var helpMenu: PopupMenu = getNodeT(PopupMenu, "vbox/menuBarControl/menuBar/Help");
+            if ((PlatformService.deviceType == PlatformDeviceType.desktop) && (OSService.getName() != "Windows")) {
+                helpMenu.systemMenuId = 4;
+            }
+            if (OSService.getName() == "macOS") {
+                helpMenu.removeItem(helpMenu.itemCount - 1);
+            }
+            helpMenu.idPressed.connect(Callable.fromFunction(function(id: Int) {
+                if (id == (helpMenu.itemCount - 1)) {
+                    Debug.error("'About' not implemented");
+                }
+                else if (id == 0) {
+                    OSService.shellOpen("https://docs.sunaba.gg");
+                }
+            }));
+
+            trace("Hello, World!");
+        }
+        catch(e: Exception) {
+            Debug.error(e.message);
+        }
     }
 
     // big dumb hack
