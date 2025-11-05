86fb9d47f2f8d36613f310bbe41e662d1ce66236
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-05 13:47:38 -0900

    Reworked EditorWidget.hx

--- Studio/Editor/src/sunaba/studio/EditorWidget.hx
+++ Studio/Editor/src/sunaba/studio/EditorWidget.hx
@@ -1,7 +1,26 @@
 package sunaba.studio;
 
 import sunaba.ui.Widget;
+import sunaba.io.IoInterface;
 
 class EditorWidget extends Widget {
-    public var parent: Editor;
+    private  var parent: Editor;
+
+    public function new(parent: Editor, area: EditorArea, ?io: IoInterface) {
+        this.parent = parent;
+        if (area == EditorArea.leftSidebar) {
+            parent.addLeftSidebarChild(this);
+        }
+        else if (area == EditorArea.rightSidebar) {
+            parent.addRightSidebarChild(this);
+        }
+        else if (area == EditorArea.workspace) {
+            parent.addWorkspaceChild(this);
+        }
+        super(io);
+    }
+
+    public function getEditor(): Editor {
+        return parent;
+    }
 }
