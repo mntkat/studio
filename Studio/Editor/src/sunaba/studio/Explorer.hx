6c1f0eb24057aa629391a1b37b0e0729400f85e4
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-06 11:22:04 -0900

    Add buttons and throbber to Project Explorer Widget

--- Studio/Editor/src/sunaba/studio/Explorer.hx
+++ Studio/Editor/src/sunaba/studio/Explorer.hx
@@ -5,8 +5,19 @@ import sunaba.ui.MenuButton;
 import sunaba.ui.TextureRect;
 import sunaba.ui.Label;
 import sunaba.ui.Tree;
+import sunaba.ui.Button;
+import sunaba.ui.Control;
 
 class Explorer extends EditorWidget {
+    var reloadButton: Button;
+    var newButton: MenuButton;
+    var hamburgerMenuButton: Button;
+
+    var throbberParent: Control;
+    var throbberRect: TextureRect;
+
+    var singleColumnTree: Tree;
+
     public override function editorInit() {
         trace("Hello, World!");
         getEditor().setLeftSidebarTabTitle(this, "Project Explorer");
@@ -18,5 +29,14 @@ class Explorer extends EditorWidget {
         getEditor().setLeftSidebarTabIcon(this, texture);
 
         load("studio://Explorer.suml");
+
+        reloadButton = getNodeT(Button, "vbox/toolbar1/hbox/reload");
+        newButton = getNodeT(MenuButton, "vbox/toolbar1/hbox/new");
+        hamburgerMenuButton = getNodeT(MenuButton, "vbox/toolbar1/hbox/hamburgerMenu");
+
+        throbberParent = getNodeT(Control, "vbox/toolbar1/hbox/throbber");
+        throbberRect = getNodeT(TextureRect, "vbox/toolbar1/hbox/throbber/textureRect");
+
+        singleColumnTree = getNodeT(Tree, "vbox/view/singleColumn/tree");
     }
 }
