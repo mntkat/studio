2aba511d8e76c89fb1c7fd8346b08f3fcc8d4aa5
Lucas C. Abbas <lucasbear9@gmail.com>
2025-11-19 14:15:25 -0900

    feat: add Plugin class for integration with the editor

---
+++ Studio/Editor/src/sunaba/studio/Plugin.hx
@@ -0,0 +1,19 @@
+package sunaba.studio;
+
+import sunaba.BaseClass;
+
+class Plugin extends BaseClass {
+    public var editor(get, default): Editor;
+    function get_editor():Editor {
+        return untyped __lua__("_G.editor");
+    }
+
+    public function new() {
+        var __this__ = this;
+        untyped __lua__("_G['plugin'] = __this__");
+    }
+
+    public function init(): Void {
+
+    }
+}
