83f9f1eda399620cf2d621f620464440e3d0aa52
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-04 09:23:33 -0900

    Refactor onReady method to include haxelib installation commands and check for editor feature

--- Studio/Splashscreen/src/Main.hx
+++ Studio/Splashscreen/src/Main.hx
@@ -1,3 +1,5 @@
+import sunaba.OSService;
+import sys.FileSystem;
 import sunaba.App;
 
 import sunaba.studio.Splashscreen;
@@ -15,5 +17,18 @@ class Main extends App {
 
     public function onReady() {
         StudioUtils.singleTonNative = rootNode.getNode("/root/StudioUtils").native;
+
+        if (!OSService.hasFeature("editor")) {
+            var haxelibPath = StudioUtils.singleton.getToolchainDirectory() + "/haxelib";
+            if (Sys.systemName() == "Windows") {
+                haxelibPath += ".exe";
+            }
+            if (!FileSystem.exists(Sys.getCwd() + "/.haxelib")) {    
+                Sys.command(haxelibPath, ["newrepo"]);
+            }
+            Sys.command(haxelibPath, ["install", StudioUtils.singleton.getBaseDirectory() + "/libsunaba.zip"]);
+            Sys.command(haxelibPath, ["install", StudioUtils.singleton.getBaseDirectory() + "/gamepak.zip"]);
+            Sys.command(haxelibPath, ["install", StudioUtils.singleton.getBaseDirectory() + "/sunaba-studio-api.zip"]);
+        }
     }
 }
