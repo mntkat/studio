1ec2e4b4da9fc93cdbfc0d47520701c2b4045d44
Lucas C. Abbas <lucasbear9@gmail.com>
2025-12-19 11:49:29 -0900

    Add SmdlFileHandler class for handling SMDL file operations

---
+++ Studio/Editor/src/sunaba/studio/fileHandlers/SmdlFileHander.hx
@@ -0,0 +1,19 @@
+package sunaba.studio.fileHandlers;
+
+import sunaba.studio.explorer.FileHandler;
+
+class SmdlFileHandler extends FileHandler {
+    public override function init() {
+        this.extension = "smdl";
+        this.iconPath = "studio://icons/16/block.png";
+    }
+
+    public override function openFile(path: String) {
+        var assetPath = StringTools.replace(path, explorer.assetsDirectory, editor.projectIo.pathUrl);
+
+        var sceneEditor = new SceneEditor(editor, EditorArea.workspace);
+        editor.setWorkspaceTabIcon(sceneEditor, explorer.loadIcon(iconPath));
+
+        sceneEditor.openPrefab(assetPath);
+    }
+}
