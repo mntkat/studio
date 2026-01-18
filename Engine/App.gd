198cd5c86b4ee76ecc3485208d7bee261e6faa01
Lucas C. Abbas <lucasbear9@gmail.com>
2026-01-18 14:32:18 -0900

    Added 'load_library' function to App.gd

--- Engine/App.gd
+++ Engine/App.gd
@@ -17,6 +17,26 @@ func init(sandboxed: bool = false, classnames: PackedStringArray = []) -> void:
 	bind_object("__ioManager", io_manager)
 	set_var("__can_debug", false)
 
+func load_library(path: String) -> void:
+	if (path.is_empty()): return
+	
+	if (!FileAccess.file_exists(path)): return
+	
+	var zipIo = IoInterfaceZip.new()
+	var zipFile = FileAccess.open(path, FileAccess.READ)
+	var zip_bytes = zipFile.get_buffer(zipFile.get_length())
+	zipIo.LoadFromBytes(zip_bytes, "temp://")
+	io_manager.Register(zipIo)
+	
+	var header_json : String = io_manager.LoadText("temp://header.json")
+	if (header_json.is_empty()):
+		_errord("header.json not found in the snb file", "Inavlid header file")
+		return
+	
+	var header: Dictionary = JSON.parse_string(header_json)
+	
+	zipIo.SetPathUrl(header["rootUrl"])
+
 func load_app(path: String) -> void:
 	if (path.is_empty()): return
 	
