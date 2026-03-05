8a541cd25fffbf2da798bbd60381d479ab8e586c
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-04 15:03:09 -0900

    Added DotNetObject Interop

---
+++ Engine/Interop/DotNetInteropService.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Godot;
+
+namespace Sunaba.Engine.Interop;
+
+public partial class DotNetInteropService : Node
+{
+    private List<Type> _types = new List<Type>();
+
+    public void AddType(Type type)
+    {
+        _types.Add(type);
+    }
+
+    public bool HasType(Type type)
+    {
+        return _types.Contains(type);
+    }
+
+    public void RemoveType(Type type)
+    {
+        _types.Remove(type);
+    }
+
+    public DotNetObject Instantiate(string className, Array args)
+    {
+        List<object> argList = new List<object>();
+        foreach (Variant arg in args)
+        {
+            argList.Add(DotNetObject.Variant2Obj(arg));
+        }
+        foreach (var type in _types)
+        {
+            if (className == type.FullName)
+            {
+                return new DotNetObject(Activator.CreateInstance(type, argList.ToArray()));
+            }
+        }
+
+        return null;
+    }
+
+    public Variant CallStatic(string className, string methodName, Array args)
+    {
+        List<object> argList = new List<object>();
+        foreach (Variant arg in args)
+        {
+            argList.Add(DotNetObject.Variant2Obj(arg));
+        }
+        foreach (var type in _types)
+        {
+            if (className == type.FullName)
+            {
+                var method = type.GetMethod(methodName);
+                if (method != null)
+                {
+                    var res = method.Invoke(null, argList.ToArray());
+                    return DotNetObject.Obj2Variant(res);
+                }
+            }
+        }
+
+        return new Variant();
+    }
+}
