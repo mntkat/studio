2c308b2ae1ea6d1be1afcdfcec72063c0506e689
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-06 14:44:35 -0900

    Updated Body tab in Character Editor

--- Studio/Editor/src/sunaba/studio/CharacterEditor.hx
+++ Studio/Editor/src/sunaba/studio/CharacterEditor.hx
@@ -1,10 +1,19 @@
 package sunaba.studio;
 
+import sunaba.core.Vector2;
 import sunaba.ui.ItemList;
+import sunaba.ui.OptionButton;
+import sunaba.ui.HSlider;
+import sunaba.ui.HBoxContainer;
+import sunaba.ui.Control;
 
 class CharacterEditor extends EditorWidget {
     public override function editorInit() {
         load("studio://CharacterEditor.suml");
+
+        var minimumSize = customMinimumSize;
+        minimumSize.y = 315;
+        customMinimumSize = minimumSize;
 
         getEditor().setDockTabTitle(this, "Character Editor");
         getEditor().setDockTabIcon(this, getEditor().loadIcon("studio://icons/16/toilet-male-edit.png"));
