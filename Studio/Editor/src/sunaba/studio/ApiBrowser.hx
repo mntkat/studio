fbb838113e2372d554c4e152d228e1925d94bfec
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-13 15:22:18 -0800

    Added functionality to the api browser

--- Studio/Editor/src/sunaba/studio/ApiBrowser.hx
+++ Studio/Editor/src/sunaba/studio/ApiBrowser.hx
@@ -1,4 +1,16 @@
 package sunaba.studio;
+import sunaba.core.Color;
+import sunaba.ui.CodeHighlighter;
+import sunaba.ui.CodeEdit;
+import sunaba.core.VariantType;
+import haxe.xml.Fast;
+import sunaba.io.IoInterface;
+import sunaba.ui.TreeItem;
+import sunaba.io.FileSystemIo;
+import haxe.io.Path;
+import sunaba.io.IoManager;
+import sunaba.ui.TabContainer;
+import sunaba.ui.Tree;
 import sunaba.ui.Widget;
 import sunaba.ui.Label;
 import sunaba.ui.Control;
@@ -14,6 +26,7 @@ import sunaba.core.Reference;
 import sunaba.core.StringArray;
 import sunaba.ui.StyleBoxEmpty;
 import sunaba.input.InputEvent;
+import sunaba.io.IoInterfaceZip;
 
 class ApiBrowser extends Widget {
     public var editor: Editor;
@@ -46,6 +59,9 @@ class ApiBrowser extends Widget {
 
     private var vbox: VBoxContainer;
     private var menuBarHbox: HBoxContainer;
+
+    private var apiTree: Tree;
+    private var apiTabs: TabContainer;
 
     public function new(editor: Editor) {
         this.editor = editor;
@@ -271,8 +287,181 @@ class ApiBrowser extends Widget {
             customTitlebar = false;
         }
 
+        apiTree = getNodeT(Tree, "vbox/hsplit/tree");
+        apiTabs = getNodeT(TabContainer, "vbox/hsplit/workspace");
+        var apiTabBar = apiTabs.getTabBar();
+        apiTabBar.tabCloseDisplayPolicy = 1;
+        apiTabBar.tabClosePressed.add((tab: Int) -> {
+            var tabControl = apiTabs.getTabControl(tab);
+            tabControl.queueFree();
+        });
+        apiTree.itemActivated.add(() -> {
+            var selected = apiTree.getSelected();
+            var metadata = selected.getMetadata(0);
+            if (metadata.getType() == VariantType.string) {
+                var codeEdit = new CodeEdit();
+                codeEdit.text = apiIo.loadText(metadata);
+                codeEdit.clearUndoHistory();
+                codeEdit.editable = false;
+                apiTabs.addChild(codeEdit);
+                var idx = apiTabs.getTabIdxFromControl(codeEdit);
+                apiTabs.setTabTitle(idx, selected.getText(0));
+                apiTabs.setTabIcon(idx, selected.getIcon(0));
+                apiTabs.currentTab = idx;
+                
+                var highlighter = new CodeHighlighter();
+                codeEdit.syntaxHighlighter = highlighter;
+
+                highlighter.numberColor = Color.code("#df7aff");
+                highlighter.symbolColor = Color.code("#9a9a9a");
+                highlighter.functionColor = Color.code("#83cdff");
+                highlighter.memberVariableColor = Color.code("#00cebe");
+                highlighter.addKeywordColor("extern", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("typedef", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("class", Color.code("#5195ff"));
+                highlighter.addKeywordColor("abstract", Color.code("#5195ff"));
+                highlighter.addKeywordColor("extends", Color.code("#5195ff"));
+                highlighter.addKeywordColor("interface", Color.code("#5195ff"));
+                highlighter.addKeywordColor("enum", Color.code("#5195ff"));
+                highlighter.addKeywordColor("function", Color.code("#5195ff"));
+                highlighter.addKeywordColor("var", Color.code("#5195ff"));
+                highlighter.addKeywordColor("new", Color.code("#5195ff"));
+                highlighter.addKeywordColor("macro", Color.code("#5195ff"));
+                highlighter.addKeywordColor("import", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("package", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("using", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("from", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("to", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("in", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("return", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("break", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("continue", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("if", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("else", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("switch", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("case", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("default", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("while", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("do", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("for", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("try", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("catch", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("throw", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("null", Color.code("#ff5fae"));
+                highlighter.addKeywordColor("true", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("false", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("this", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("super", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("untyped", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("dynamic", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("override", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("implements", Color.code("#ff9d00"));
+                highlighter.addKeywordColor("private", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("protected", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("public", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("static", Color.code("#9f6eff"));
+                highlighter.addKeywordColor("trace", Color.code("#ff8080"));
+                highlighter.addColorRegion("/*", "*/", Color.code("#9bda7b"), false);
+                highlighter.addColorRegion("//", "", Color.code("#9bda7b"), true);
+                highlighter.addColorRegion("\"", "\"", Color.code("#9bda7b"), false);
+                highlighter.addColorRegion("'", "'", Color.code("#9bda7b"), false);
+            }
+        });
+
+        loadAllApis();
     }
 
+    private var haxelibIo: FileSystemIo = null;
+    private var apiIo: IoManager = null;
+
+    public function loadAllApis() {
+        if (apiIo == null) {
+            apiIo = new IoManager();
+        }
+        if (haxelibIo == null) {
+            var haxelibDir = Path.addTrailingSlash(Sys.getCwd()) + ".haxelib/";
+            haxelibIo = new FileSystemIo();
+            haxelibIo.open(haxelibDir, "haxelib://");
+            apiIo.register(haxelibIo);
+        }
+
+        var rootItem = apiTree.createItem();
+        apiTree.hideRoot = true;
+
+        var libraries = haxelibIo.getFileListAll("/", false);
+
+        for (i in 0...libraries.size()) {
+            var library: String = libraries.get(i);
+            trace(library);
+            var apiName = library.split("/")[2];
+            trace(apiName);
+            var dirPath = Path.addTrailingSlash(library) + "0,0,0/";
+            if (haxelibIo.directoryExists(dirPath)) {
+                loadApi(dirPath, apiName);
+            }
+        }
+    }
+
+    public function loadApi(path: String, apiName: String) {
+        
+        var item = apiTree.createItem();
+        item.setText(0, apiName);
+        item.setIcon(0, editor.loadIcon("studio://icons/16/haxe.png"));
+        item.collapsed = true;
+
+        var pathIo = new FileSystemIo();
+        pathIo.open(haxelibIo.getFilePath(path), apiName + "://");
+        apiIo.register(pathIo);
+
+        var directories = pathIo.getFileListAll("/", false);
+
+        for (i in 0...directories.size()) {
+            var dir = directories.get(i);
+            recurseDir(dir, item, pathIo);
+        }
+
+        var hxFiles = pathIo.getFileListAll(".hx", false);
+
+        for (i in 0...hxFiles.size()) {
+            var hxFilePath: String = hxFiles.get(i);
+            var hxFileName = hxFilePath.split("/").pop().split(".")[0];
+            var codeItem = apiTree.createItem(item);
+            codeItem.setText(0, hxFileName);
+            codeItem.setIcon(0, editor.loadIcon("studio://icons/16/haxe.png"));
+            codeItem.setMetadata(0, hxFilePath);
+        }
+    }
+
+    private function recurseDir(path: String, parent: TreeItem, pathIo: IoInterface) {
+        var item = apiTree.createItem(parent);
+        var pathArr = path.split("/");
+        var packageName = pathArr[pathArr.length - 1];
+        if (packageName == "") {
+            packageName = pathArr[pathArr.length - 2];
+        }
+
+        item.setText(0, packageName);
+        item.setIcon(0, editor.loadIcon("studio://icons/16/blue-folder.png"));
+        item.collapsed = true;
+
+        var directories = pathIo.getFileList(path, "/", false);
+
+        for (i in 0...directories.size()) {
+            var dir = directories.get(i);
+            recurseDir(dir, item, pathIo);
+        }
+
+        var hxFiles = pathIo.getFileList(path, ".hx", false);
+
+        for (i in 0...hxFiles.size()) {
+            var hxFilePath: String = hxFiles.get(i);
+            var hxFileName = hxFilePath.split("/").pop().split(".")[0];
+            var codeItem = apiTree.createItem(item);
+            codeItem.setText(0, hxFileName);
+            codeItem.setIcon(0, editor.loadIcon("studio://icons/16/document-code.png"));
+            codeItem.setMetadata(0, hxFilePath);
+        }
+    }
 
     public override function onProcess(delta:Float) {
         if (OSService.getName() == "macOS") {
