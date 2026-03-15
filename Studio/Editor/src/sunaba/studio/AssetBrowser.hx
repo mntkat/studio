461a9d0447f8d45d43c5fd880a29f60159c10b82
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-15 12:34:32 -0800

    Added variables for nodes in AssetBrowser.hx

--- Studio/Editor/src/sunaba/studio/AssetBrowser.hx
+++ Studio/Editor/src/sunaba/studio/AssetBrowser.hx
@@ -1,10 +1,49 @@
 package sunaba.studio;
 
+import sunaba.ui.ItemList;
+import sunaba.ui.Tree;
+import sunaba.ui.TreeItem;
+import sunaba.ui.MenuButton;
+import sunaba.ui.Button;
+import sunaba.ui.LineEdit;
+
 class AssetBrowser extends EditorWidget {
+    var backButton: Button;
+    var forwardButton: Button;
+    var upButton: Button;
+    var reloadButton: Button;
+
+    var addressBar: LineEdit;
+    var searchBar: LineEdit;
+
+    var newButton: MenuButton;
+
+    var tree: Tree;
+    var itemList: ItemList;
+
+    var currentDir = "";
+    var previousDirs: Array<String> = [];
+    var nextDirs: Array<String> = [];
+
+    var pathTreeItemMap: Map<String, TreeItem> = new Map();
+
     public override function editorInit() {
         load("studio://AssetBrowser.suml");
 
         getEditor().setDockTabTitle(this, "Assets");
         getEditor().setDockTabIcon(this, getEditor().loadIcon("studio://icons/16/blue-folder-open-image.png"));
+
+        backButton = getNodeT(Button, "vbox/toolbar/hbox/back");
+        forwardButton = getNodeT(Button, "vbox/toolbar/hbox/forward");
+        upButton = getNodeT(Button, "vbox/toolbar/hbox/up");
+        reloadButton = getNodeT(Button, "vbox/toolbar/hbox/reload");
+
+        addressBar = getNodeT(LineEdit, "vbox/toolbar/hbox/addressBar");
+        searchBar = getNodeT(LineEdit, "vbox/toolbar/hbox/searchBar");
+
+        newButton = getNodeT(MenuButton, "vbox/toolbar/hbox/new");
+
+        tree = getNodeT(Tree, "vbox/toolbar/view/hsplit/tree");
+        itemList = getNodeT(ItemList, "vbox/toolbar/view/hsplit/itemList");
     }
 }
