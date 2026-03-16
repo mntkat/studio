53ca91d3809152d98871b629562ddb4864acfaf2
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-16 10:41:08 -0800

    Implement directory tree structure with coroutine support in AssetBrowser

--- Studio/Editor/src/sunaba/studio/AssetBrowser.hx
+++ Studio/Editor/src/sunaba/studio/AssetBrowser.hx
@@ -1,5 +1,7 @@
 package sunaba.studio;
 
+import lua.Coroutine;
+import sunaba.core.Variant;
 import sunaba.io.IoManager;
 import sunaba.ui.ItemList;
 import sunaba.ui.Tree;
@@ -53,13 +55,97 @@ class AssetBrowser extends EditorWidget {
         buildTreeRoot();
     }
 
+    var dirIconTexture: ImageTexture;
+
     public function buildTreeRoot() {
         var ioManager: IoManager = cast io;
 
-        var pathUrls = ioManager.getPathUrls();
+        var pathUrls: Array<String> = ioManager.getPathUrls();
 
         tree.clear();
         var root = tree.createItem();
+
+        var dirIconBytes = io.loadBytes("studio://icons/16/blue-folder.png");
+        var dirIconImage = new Image();
+        dirIconImage.loadPngFromBuffer(dirIconBytes);
+        dirIconTexture = ImageTexture.createFromImage(dirIconImage);
+
+        var pathUrlDirIconBytes = io.loadBytes("studio://icons/16/blue-folder.png");
+        var pathUrlDirIconImage = new Image();
+        pathUrlDirIconImage.loadPngFromBuffer(pathUrlDirIconBytes);
+        var pathUrlDirIconTexture = ImageTexture.createFromImage(pathUrlDirIconImage);
         
+        for (pathUrl in pathUrls) {
+            var pathUrlItem = tree.createItem(root);
+            pathUrlItem.setText(0, StringTools.replace(pathUrl, "://", ""));
+            pathUrlItem.setIcon(0, pathUrlDirIconTexture);
+            pathUrlItem.setMetadata(0, pathUrl);
+
+            var dirTreeCoroutine = Coroutine.create(() -> {
+                Coroutine.yield();
+                var dirs: Array<Variant> = io.getFileList(pathUrl, "/", false);
+                Coroutine.yield();
+                for (dir in dirs) {
+                    Coroutine.yield();
+                    if (dir == pathUrl || dir == pathUrl + "/") {
+                        continue;
+                    }
+                    Coroutine.yield();
+                    buildDirTree(dir, pathUrlItem);
+                    Coroutine.yield();
+                }
+                Coroutine.yield();
+            });
+            dirTreeCoroutines.push(dirTreeCoroutine);
+            Coroutine.resume(dirTreeCoroutine);
+        }
+    }
+
+    var dirTreeCoroutines : Array<Coroutine<()->Void>> = new Array();
+
+    private function buildDirTree(dir: String, parent: TreeItem) {
+        var dirTreeCoroutine = Coroutine.create(() -> {
+            Coroutine.yield();
+            var item = tree.createItem(parent);
+            Coroutine.yield();
+            var pathArray = dir.split("/");
+            var dirName = pathArray[pathArray.length - 1];
+            if (dirName == "") {
+                dirName = pathArray[pathArray.length - 2];
+            }
+            if (dirName != "") {
+                Coroutine.yield();
+                item.setText(0, dirName);
+                Coroutine.yield();
+                item.setIcon(0, dirIconTexture);
+                Coroutine.yield();
+                item.setMetadata(0, dir);
+                Coroutine.yield();
+                item.collapsed = true;
+                Coroutine.yield();
+                var subDirs: Array<Variant> = io.getFileList(dir, "/", false);
+                Coroutine.yield();
+                for (subDir in subDirs) {
+                    Coroutine.yield();
+                    buildDirTree(subDir, item);
+                    Coroutine.yield();
+                }
+            Coroutine.yield();
+            }
+            
+        });
+        dirTreeCoroutines.push(dirTreeCoroutine);
+        Coroutine.resume(dirTreeCoroutine);
+    }
+
+    public override function onProcess(delta:Float) {
+        for (dirTreeCoroutine in dirTreeCoroutines) {
+            if (Coroutine.status(dirTreeCoroutine) != CoroutineState.Dead) {
+                Coroutine.resume(dirTreeCoroutine);
+            }
+            else {
+                dirTreeCoroutines.remove(dirTreeCoroutine);
+            }
+        }
     }
 }
