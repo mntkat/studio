8c906667a5a158e9024802b6cb4209d07f68b844
Lucas C. Abbas <lucasbear9@gmail.com>
2026-03-16 18:06:35 -0800

    Fixed IoInterfaceZip

--- Engine/IoInterfaceZip.cs
+++ Engine/IoInterfaceZip.cs
@@ -9,246 +9,73 @@ using System.Linq;
 namespace Sunaba.Engine;
 
 [GlobalClass]
-public partial class IoInterfaceZip : IoInterface
+public partial class IoInterfaceZip : SystemIoBase
 {
-	ZipArchive zipArchive;
+    public String Path;
+    public String AltPath;
+    public String AltPath2;
 
-	IoInterfaceZip()
-	{
-	}
+    public IoInterfaceZip()
+    {
+    }
 
-	public void LoadFromPath(string path, string pathUrl)
-	{
-		PathUrl = pathUrl;
-		if (path.EndsWith(".snb") || path.EndsWith(".slib") || path.EndsWith(".zip"))
-		{
-			zipArchive = ZipFile.OpenRead(path);
-		}
-		else
-		{
-			throw new Exception("Invalid Zip File");
-		}
-	}
+    public void Open(String path, String pathUrl)
+    {
+        var outputDir = System.IO.Path.GetTempPath();
+        if (!outputDir.EndsWith("/") && !path.EndsWith("\\"))
+        {
+            outputDir += "/";
+        }
 
-	public void LoadFromBytes(byte[] buffer, string pathUrl)
-	{
-		PathUrl = pathUrl;
-		MemoryStream ms = new MemoryStream(buffer);
-		zipArchive = new ZipArchive(ms);
-	}
+        outputDir += path.GetFile() + "_output/";
+        if (Directory.Exists(outputDir))
+        {
+            Directory.Delete(outputDir, true);
+        }
+        ZipFile.ExtractToDirectory(path, outputDir);
 
-	public override string GetFilePath(string path)
-	{
-		if (path.StartsWith(PathUrl))
-		{
-			path = path.Replace(PathUrl, "");
-			if (path == null)
-			{
-				throw new Exception("Path Conversion Error");
-			}
-		}
-		else if (path.StartsWith("./"))
-		{
-			path = path.Replace("./", "");
-			if (path == null)
-			{
-				throw new Exception("Path Conversion Error");
-			}
-		}
-		if (path.Contains("\\"))
-		{
-			path = path.Replace("\\/", "/");
-			path = path.Replace("\\", "/");
-		}
-		if (path.Contains("/"))
-		{
-			path = path.Replace("/", "\\");
-		}
-		path = path.Replace("\\", "/");
-		return path;
-	}
+        Path = outputDir;
+        Path = Path.Replace("\\/", "/");
+        Path = Path.Replace("/\\", "/");
+        Path = Path.Replace("\\", "/");
+        PathUrl = pathUrl;
+        AltPath = outputDir.Replace("/", "\\");
+        AltPath2 = outputDir.Replace("\\", "/");
+    }
 
-	public string SanitizePath(string path)
-	{
-		path = path.Replace("\\/", "\\");
-		path = path.Replace("\\", "/");
-		return path;
-	}
+    public override string GetFilePath(string path)
+    {
 
-	public override string LoadText(string assetPath)
-	{
-		string path = GetFilePath(assetPath);
-		ZipArchiveEntry entry = zipArchive.GetEntry(path);
-		if (entry == null)
-		{
-			return null;
-		}
-		using (StreamReader reader = new StreamReader(entry.Open()))
-		{
-			return reader.ReadToEnd();
-		}
-	}
+        if (path.StartsWith(PathUrl))
+        {
+            path = path.Replace(PathUrl, Path);
+            if (path == null)
+            {
+                throw new Exception("Path Conversion Error");
+            }
+        }
+        else if (path.StartsWith("./"))
+        {
+            path = path.Replace("./", Path);
+            if (path == null)
+            {
+                throw new Exception("Path Conversion Error");
+            }
+        }
 
-	public override byte[] LoadBytes(string assetPath)
-	{
-		string path = GetFilePath(assetPath);
-		ZipArchiveEntry entry = zipArchive.GetEntry(path);
-		if (entry == null)
-		{
-			return null;
-		}
-		using (MemoryStream ms = new MemoryStream())
-		{
-			entry.Open().CopyTo(ms);
-			return ms.ToArray();
-		}
-	}
+        if (path.Contains("\\/"))
+        {
+            path = path.Replace("\\/", "/");
+            path = path.Replace("\\", "/");
+        }
 
-	private Array<String> _getFiles()
-	{
-		var entries = zipArchive.Entries;
-		Array<String> assets = [];
-		foreach (var zipArchiveEntry in entries)
-		{
-			assets.Add(zipArchiveEntry.FullName);
-		}
+        return path;
+    }
 
-		return assets;
-	}
-
-	public override Array<string> GetFileList(string path, string extension = "", bool recursive = true)
-	{
-		var fileList = new Array<string>();
-		var files = _getFiles();
-		var path2 = GetFilePath(path);
-		if (!path2.EndsWith("/") && path2 != "")
-			path2 += "/";
-		
-		if (extension != "" && extension != "/")
-		{
-			foreach (var file in files)
-			{
-				var filePath = PathUrl + file;
-				if (file.StartsWith(path2))
-				{
-					if (file.EndsWith(extension))
-					{
-						if (path2 == "")
-						{
-							fileList.Add(filePath);
-							continue;
-						}
-						if (!file.Replace(path2, "").Contains("/"))
-						{
-							fileList.Add(filePath);
-						}
-					}
-				}
-			}
-
-			if (recursive)
-			{
-				var subDirs = GetFileList(path, "/", false);
-				foreach (var subDir in subDirs)
-				{
-					var subDirFiles = GetFileList(subDir, extension);
-					foreach (var subDirFile in subDirFiles)
-					{
-						if (!fileList.Contains(subDirFile))
-						{
-							fileList.Add(subDirFile);
-						}
-					}
-				}
-			}
-		}
-		else if (extension == "/")
-		{
-			foreach (var file in files)
-			{
-				var filePath = PathUrl + file;
-				var baseDir = file.GetBaseDir();
-				if (String.IsNullOrEmpty(baseDir)) continue;
-				if (baseDir.StartsWith("/")) {
-					baseDir = baseDir.Substring(1);
-				}
-				var dirPath = PathUrl + baseDir + "/";
-				if (dirPath == path) continue;
-				if (dirPath.StartsWith(path) || path == PathUrl)
-				{
-					string remaining = dirPath.Replace(path, "");
-					if (remaining != "")
-					{
-						if (remaining.EndsWith("/")) {
-							remaining = remaining.Substring(0, remaining.Length - 1);
-						}
-						if (!remaining.Contains("/")) 
-						{
-							if (!fileList.Contains(dirPath))
-							{
-								fileList.Add(dirPath);
-							}
-						}		
-					}
-				}
-			}
-			if (recursive)
-			{
-				var subDirs = GetFileList(path, "/", false);
-				foreach (var subDir in subDirs)
-				{
-					var subDirFiles = GetFileList(subDir, "/");
-					foreach (var subDirFile in subDirFiles)
-					{
-						if (!fileList.Contains(subDirFile))
-						{
-							fileList.Add(subDirFile);
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			foreach (var file in files)
-			{
-				var filePath = PathUrl + file;
-				if (file.StartsWith(path2))
-				{
-					if (!file.Replace(path2, "").Contains("/"))
-					{
-						fileList.Add(filePath);
-					}
-				}
-			}
-			if (recursive)
-			{
-				var subDirs = GetFileList(path, "/", false);
-				foreach (var subDir in subDirs)
-				{
-					var subDirFiles = GetFileList(subDir);
-					foreach (var subDirFile in subDirFiles)
-					{
-						if (!fileList.Contains(subDirFile))
-						{
-							fileList.Add(subDirFile);
-						}
-					}
-				}
-			}
-		}
-		return fileList;
-	}
-	public override bool DirectoryExists(string path)
-	{
-		path = GetFilePath(path);
-		foreach (var entry in zipArchive.Entries)
-		{
-			if (entry.FullName.StartsWith(path))
-				return true;
-
-		}
-
-		return false;
-	}
+    public override string GetFileUrl(string path){
+        path = path.Replace(Path, PathUrl);
+        path = path.Replace(AltPath, PathUrl);
+        path = path.Replace(AltPath2, PathUrl);
+        return path;
+    }
 }
